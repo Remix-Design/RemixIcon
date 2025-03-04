f3e4fef492e230b63ad03f671df91cf1b0ef6486
balogunofafrica <balogunakanbi.k@gmail.com>
2025-03-04 07:54:58 +0100

    update

--- icon-pack/rds/check-mark.tsx
+++ icon-pack/rds/check-mark.tsx
@@ -1,17 +1,11 @@
-import { memo } from "react";
-import type { IconProps } from "../../types";
-import { Svg, Path, Rect } from "react-native-svg";
+import { memo } from 'react'
+import type { IconProps } from '../../types'
+import { Svg, Path, Rect } from 'react-native-svg'
 
 const Icon = (props: IconProps) => {
-  const { color = "black", size = 24, ...otherProps } = props;
+  const { color = 'black', size = 24, ...otherProps } = props
   return (
-    <Svg
-      viewBox="0 0 20 20"
-      fill={color}
-      height={size}
-      width={size}
-      {...otherProps}
-    >
+    <Svg viewBox="0 0 20 20" fill={color} height={size} width={size} {...otherProps}>
       <Rect width="20" height="20" rx="10" stroke="none" strokeWidth="0" />
       <Path
         d="M6 11.2667L8.31111 13.4L14 7"
@@ -20,9 +14,9 @@ const Icon = (props: IconProps) => {
         strokeLinejoin="round"
       />
     </Svg>
-  );
-};
+  )
+}
 
-Icon.displayName = "CheckMark";
+Icon.displayName = 'CheckMark'
 
-export const CheckMark = memo(Icon);
+export const CheckMark = memo(Icon)
